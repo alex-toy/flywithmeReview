6f04e321abe6410438f1e60f5f092879725142f8
alexei <alexei.80@hotmail.fr>
2018-03-10 06:33:44 +0100

    modify PDOFactory

--- lib/OCFram/PDOFactory.php
+++ lib/OCFram/PDOFactory.php
@@ -6,14 +6,14 @@ class PDOFactory
   public static function getMysqlConnexion()
   {
     
-    $db = new \PDO('mysql:host=localhost;dbname=news;charset=utf8', 'root', 'root');
+    $bdd = new \PDO('mysql:host=localhost;dbname=news;charset=utf8', 'root', 'root');
 
     
     
-    //$db = new \PDO('mysql:host=db725682133.db.1and1.com;dbname=db725682133;charset=utf8', 'dbo725682133', 'Colmoschin.80');
-    $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    //$bdd = new \PDO('mysql:host=db725682133.db.1and1.com;dbname=db725682133;charset=utf8', 'dbo725682133', 'Colmoschin.80');
+    $bdd->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
  
-    return $db;
+    return $bdd;
   }
 }
 
